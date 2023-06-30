603a427b4efe3b275e196732122cd5992479fbe8
root <root@guest.guest>
2023-06-30 14:37:50 +0000

    reverted to local storage

--- markov/markov.js
+++ markov/markov.js
@@ -12,23 +12,24 @@ function loadChain(callback) {
   xhr.send(null);
 }
 
-// Call loadChain to load the ngram data from the JSON file
+// Call loadNgram to load the ngram data from the JSON file
 loadChain(function(ngram) {
   // Use the ngram data to generate the markov chain
-  // word_pair is a single string of the form word1_word2
-  var wordPair = Object.keys(ngram)[Math.floor(Math.random() * Object.keys(ngram).length)];
-  var wordArr = wordPair.split("_")
-  var chain = wordArr[0]+" "+wordArr[1]+" "
+    // word_pair is a single string of the form word1_word2
+  var word_pair = Object.keys(ngram)[Math.floor(Math.random() * Object.keys(ngram).length)];
+  var wordArr = word_pair.split("_")
+    var chain = wordArr[0]+" "+wordArr[1]+" "
+  
 
   //get chain
   while(true){ 
-    if(!(wordPair in ngram)){
+    if(!(word_pair in ngram)){
       break;
     };
-    var third = ngram[wordPair][Math.floor(Math.random() * ngram[wordPair].length)];
+    var third = ngram[word_pair][Math.floor(Math.random() * ngram[word_pair].length)];
     chain += third + " ";
-    wordArr = wordPair.split("_")
-    wordPair = wordArr[1]+"_"+third;
+    wordArr = word_pair.split("_")
+    word_pair = wordArr[1]+"_"+third;
   };
   
   output.innerHTML += chain;
