60b0638a136c51c7765d833a4c514b8f11af3caa
Minimac21 <mac.holland.d@gmail.com>
2024-10-05 21:40:43 -0500

    moving corpuses + save button

--- markov/make_ngram.py
+++ markov/make_ngram.py
@@ -1,33 +1,46 @@
 #https://onlinegdb.com/IrQRJkyX0
 #https://files.catbox.moe/fj3568.py
 
-#if ur lucky it will combine sailor moon eva n lain lore
 import sys
-assert len(sys.argv) == 2
-corpusFile = open(sys.argv[1],"r")
-corpus = corpusFile.read()
 import re
-import random
 import json
+from os import path
+
+try:
+    assert len(sys.argv) == 3
+except Exception as e:
+    raise Exception("This script takes two arguments; the path to the corpus, and the n in ngram")
+
+corpusFileName = sys.argv[1]
+n=sys.argv[2]
+try:
+    n=int(n)
+except Exception as e:
+    raise Exception("The second argument must be an integer, which is the n in ngram")
+
+corpusFile = open(corpusFileName,"r")
+
+corpus = corpusFile.read()
 ngram = {}
 corpus = corpus.lower().replace('"','').replace("'",'').replace('\n',' ').replace(')','').replace('(','').replace('[','').replace(']','').replace('’','').replace("“",'').replace("”",'').replace("\r"," ").replace(",","").replace(";","").replace("\u2014"," ").replace("\u2018","").replace("_","")
-     
+
 for sentence in re.split(r"[.?!]", corpus):
-    
+
+    words = sentence.split(" ")
     #iterate through the words of the sentence
-    for i in range(2, len(sentence.split(' '))):
-        wordOne = sentence.split(' ')[i - 2]
-        wordTwo = sentence.split(' ')[i - 1]
-        wordThree = sentence.split(' ')[i]
-        if "" in [wordOne,wordTwo,wordThree]:
+    for i in range(n-1, len(words)):
+        gram = words[i+1-n:i+1]
+        if "" in gram:
             continue
-        word_pair = wordOne + "_" + wordTwo       
-        if word_pair not in ngram:
-            ngram[word_pair] = []
-        ngram[word_pair].append(wordThree)
+        pre_gram = "_".join(gram[:-1])
+        if pre_gram not in ngram:
+            ngram[pre_gram] = []
+        ngram[pre_gram].append(gram[-1])
+
 corpusFile.close()
 
-ngramFile = open("joyce-ngram.json","w")
+ngramFileName=path.basename(corpusFileName).split(".")[0]
+ngramFile = open(f"{n}grams/{ngramFileName}-ngram.json","w")
 ngramJson = json.dumps(ngram, indent=2)
 ngramFile.write(ngramJson)
 ngramFile.close()
